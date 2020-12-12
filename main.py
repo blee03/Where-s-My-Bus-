6c725706a58d44c99ab9922c1e4004d0728d4904
btlee264855 <75905365+btlee264855@users.noreply.github.com>
2020-12-12 12:17:52 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import http.client, urllib.request, urllib.parse, urllib.error, base64
+
+headers = {
+    # Request headers
+    'Ocp-Apim-Subscription-Key': '0466719b080e49999811b70f9cd28e85',
+}
+
+params = urllib.parse.urlencode({
+    # Request parameters
+    '$filter': '{String}',
+    '$top': '{string}',
+    '$skip': '{string}',
+    '$format': 'json',
+    '$orderby': '{String}',
+})
+
+try:
+    conn = http.client.HTTPSConnection('hacktj2020api.eastbanctech.com')
+    conn.request("GET", "/transitiq/Routes?%s" % params, "{body}", headers)
+    response = conn.getresponse()
+    data = response.read()
+    print(data)
+    conn.close()
+except Exception as e:
+    print("[Errno {0}] {1}".format(e.errno, e.strerror))
