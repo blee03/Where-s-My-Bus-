b1e52814bfe3ca29c5422456021d06d1d720a4e7
btlee264855 <75905365+btlee264855@users.noreply.github.com>
2020-12-13 12:02:10 -0500

    more attempts to reroute

--- webapp.py
+++ webapp.py
@@ -1,24 +1,21 @@
 from flask import Flask, render_template, request, redirect
 from apicalls import grab_routes, grab_stops
 app = Flask(__name__)
+val = 0
 #route page render
 @app.route('/')
 def routes():
     routelist = []
     routelist = grab_routes('1')
     return render_template('index.html', routelist=routelist)
-
-@app.route('/', methods=['POST'])
-def user_route():
-    val = request.form['routechosen']
-    return val
 @app.route('/move', methods=['POST'])
 def movetostops():
+    val = str(request.form['route_chosen'])
     return redirect('/stops')
 @app.route('/stops')
 def stops():
     stoplist = []
-    stoplist=grab_stops(user_route())
+    stoplist=grab_stops(int(val))
     return render_template('location.html', stoplist=stoplist)
 
 if __name__ == '__main__':
