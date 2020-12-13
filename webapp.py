11faa057e43d6b3e15db7c1f44faa8b876d4713f
btlee264855 <75905365+btlee264855@users.noreply.github.com>
2020-12-12 23:05:16 -0500

    fixed more front end issues

--- webapp.py
+++ webapp.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+    test = "Find out when your bus will arrive in Houston, TX."
+    return render_template('index.html', test = test)
 if __name__ == '__main__':
     app.run()
