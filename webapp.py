73a930f644765b208fdc4b13919ca429e76c47d2
btlee264855 <75905365+btlee264855@users.noreply.github.com>
2020-12-12 23:12:06 -0500

    passes data from main.py to index.html

--- webapp.py
+++ webapp.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
+from main import callroute
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    test = "Find out when your bus will arrive in Houston, TX."
+    test = callroute
     return render_template('index.html', test = test)
 if __name__ == '__main__':
     app.run()
